15b0e7d0d219146efa54ae0e44ecd1db122242c5
Brian Flores <brianhdez824@gmail.com>
2024-10-27 16:43:24 -0600

    First: uploading project

---
+++ data/index.ts
@@ -0,0 +1,218 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Priorizo ​​la colaboración con el cliente, fomentando la comunicación abierta. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Me ajusto a cualquier horario.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Mi tech stack",
+    description: "Intento mejorar",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta de la tecnología y apasionado por el desarrollo.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "De la idea a la realidad: sistemas únicos diseñados para ti.",
+    description: "Mi visión",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "¿Quieres empezar un proyecto juntos?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Sitio Web B+B Arquitectos",
+    des: "Sitio web personalizado para despacho de arquitectos B+B en Tula de Allende.",
+    img: "/arquitectos.png",
+    iconLists: ["/php.svg", "/ts.svg", "/tail.svg", "/bootstrap-5-1.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Plataforma Atzco",
+    des: "Sistema de gestión de RH diseñado para Constructora Atzco.",
+    img: "/atzco.png",
+    iconLists: ["/php.svg", "/ts.svg", "/tail.svg", "/bootstrap-5-1.svg"],
+    link: "https://atzcorh.net/HR_System/",
+  },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  //   link: "/ui.aiimg.com",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "/ui.apple.com",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Colaborar con Brian fue una experiencia muy satisfactoria. Se mostró siempre comprometido y responsable, asegurándose de que cada detalle del proyecto se completara a tiempo y con gran calidad. El resultado final cumplió perfectamente con nuestras expectativas. Lo recomiendo sin dudarlo.",
+    name: "Arq. Benjamín Hernández",
+    title: "Director General de B+B Arquitectos",
+  },
+  {
+    quote:
+      "Brian desarrolló el sistema para Atzco como parte de su proyecto de estadía, y desde el inicio demostró un gran compromiso y rapidez. Se integró perfectamente con nuestro equipo, atendiendo cada solicitud con responsabilidad y eficiencia. El sistema ha sido un éxito total, optimizando nuestros procesos y cumpliendo con todas nuestras expectativas. Estamos muy satisfechos con los resultados y su trabajo.",
+    name: "Ing. Alan Tellez",
+    title: "Encargado del Área de Sistemas",
+  },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+  // {
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   name: "Michael Johnson",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Desarrollador Full Stack",
+    desc: "Desarrollé una plataforma de gestión de Recursos Humanos para una empresa, integrando PHP y TypeScript para mejorar la eficiencia interna.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Desarrollador Freelancer",
+    desc: "Realizo proyectos personalizados, creando sitios web y aplicaciones eficientes y personalizadas, siempre entregando a tiempo.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp4.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "Freelance App Dev Project",
+  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp2.svg ",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
